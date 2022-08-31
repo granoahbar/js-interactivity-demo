97eb5816187fcd48b345bc8e92c67bc18ea8b423
Noah Humphreys <noahlorinhumphreys@gmail.com>
2022-08-31 13:30:07 -0600

    end of demo

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const plusBtn = document.querySelector('#plus-btn')
 const minusBtn = document.querySelector('#minus-btn')
 const resetBtn = document.querySelector('#reset-btn')
+const counter = document.querySelector('#counter')
+const themeBtns = document.querySelectorAll('.theme-buttons')
 
 // Step 2: Write the function
 
@@ -10,21 +12,39 @@ let count = 0
 
 const increase = () => {
     count++
+    counter.textContent = count
     console.log(count)
 }
 
 const decrease = () => {
     count--
+    counter.textContent = count
     console.log(count)
 }
 
-const reset = () => {
+const reset = (event) => {
+    console.log(event)
     count = 0
+    counter.textContent = count
     console.log(count)
 }
+
+const selectTheme = (event) => {
+    console.log(event.target.textContent)
+    const theme = event.target.textContent
+    document.querySelector('body').className = theme
+    document.querySelector('main').className = theme
+}
+
 
 // Step 3: Combines step 1 and 2 using an Event Listener
 
 plusBtn.addEventListener('click', increase)
 minusBtn.addEventListener('click', decrease)
 resetBtn.addEventListener('click', reset)
+
+for(let i = 0; i < themeBtns.length; i++){
+
+    themeBtns[i].addEventListener('click', selectTheme)
+    console.log(themeBtns[i].textContent)
+}
